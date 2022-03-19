74b90742b2756e73ce7c7c28af4498671ee7c7c1
CodexTheSexyMan <101730305+CodexTheSexyMan@users.noreply.github.com>
2022-03-18 23:10:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE HTML>
+
+<html>
+  <head>
+
+
+  </head>
+<body>
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="buttons">
+  <br>
+<a href='https://forums.lightnetworks.xyz'><button class="forumsbutton">Forums!</button></a>
+<br>
+<a href='https://bans.lightnetworks.xyz'><button class="bansbutton">Bans!</button></a>
+  <br>
+  <a href='https://donate.lightnetworks.xyz'><button class="donatebutton">Donate!</button></a>
+  <br>
+  <a href='https://donate.lightnetworks.xyz'><button class="steambutton">Steam Workshop!</button></a>
+  <br>
+</div>
+<br>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Neonderthaw&display=swap');
+
+  html {
+    background-image: url('logo.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .buttons {
+  }
+
+  /* Control the left side */
+  .left {
+    left: 0;
+  }
+
+  /* Control the right side */
+  .right {
+    right: 0;
+    background-color: gray;
+  }
+
+  /* If you want the content centered horizontally and vertically */
+  .centered {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    background-image: url('logo.png');
+    background-repeat: no-repeat;
+    background-size: 100% 50%;
+  }
+
+  /* Style the image inside the centered container, if needed */
+
+  .forumsbutton {
+    display: block;
+    width: 100%;
+    border: none;
+    background-color: transparent;
+    color: white;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    text-align: center;
+    color: white;
+    border-radius: 15px 15px;
+    font-family: 'Neonderthaw', cursive;
+  }
+
+  .bansbutton {
+    display: block;
+    width: 100%;
+    border: none;
+    background-color: transparent;
+    color: white;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    text-align: center;
+    color: white;
+    border-radius: 15px 15px;
+    font-family: 'Neonderthaw', cursive;
+  }
+
+  .donatebutton {
+    display: block;
+    width: 100%;
+    border: none;
+    background-color: transparent;
+    color: white;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    text-align: center;
+    color: white;
+    border-radius: 15px 15px;
+    font-family: 'Neonderthaw', cursive;
+    font: Neonderthaw;
+  }
+
+  .steambutton {
+    display: block;
+    width: 100%;
+    border: none;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    text-align: center;
+    color: white;
+    border-radius: 15px 15px;
+    background-color: transparent;
+    color: white;
+    outline-color: white;
+    font-family: 'Neonderthaw', cursive;
+    font:
+  }
+
+
+</style>
